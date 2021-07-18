5b4842e1d2115f41c2b1bf3b6a66b788ea7b6e44
Pramod <c4pramod@hotmail.com>
2021-07-18 20:07:17 +0800

    Enhanced component for consuming api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TaskMgmt from './TaskMgmt';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import TaskMgmt from './task/TaskMgmt';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <TaskMgmt></TaskMgmt>
   </React.StrictMode>,
   document.getElementById('root')
